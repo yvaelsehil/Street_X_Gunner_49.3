8ee5ff37262b0d090f723c91f224cf3bfcc3614c
bisneu <carambar93@live.fr>
2016-07-01 16:14:43 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/example/bisneu/street_x_gunner_493/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.bisneu.street_x_gunner_493;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
