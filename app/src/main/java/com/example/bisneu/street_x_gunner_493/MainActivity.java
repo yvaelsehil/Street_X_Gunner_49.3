f14d248297187d08f1757b8647da57a2b36733ed
bisneu <carambar93@live.fr>
2016-07-06 14:20:11 +0200

    interface graphique de FOU !

--- app/src/main/java/com/example/bisneu/street_x_gunner_493/MainActivity.java
+++ app/src/main/java/com/example/bisneu/street_x_gunner_493/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bisneu.street_x_gunner_493;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // commentaire nicolas teub
+        View bouton1 = findViewById(R.id.Img1);
+        View bouton2 = findViewById(R.id.Img2);
+        // click listenet pour le bouton 1
+        bouton1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        System.out.println("Tu as appuyé sur le bouton 1");
+                    }
+                }
+        );
+
+        // click listener pour le bouton 2
+        bouton2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        System.out.println("Tu as appuyé sur le bouton 2");
+                    }
+                }
+        );
     }
 }
